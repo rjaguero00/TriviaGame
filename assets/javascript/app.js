c2a7ec647bad6ffc8d49c741db75d5dd2397449e
Roberto aguero <rjaguero00@Robertos-MacBook-Air.local>
2018-02-23 11:13:51 -0700

    Timer and check ans

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,11 @@ $(document).ready(function() {
 
   var intervalId;
   var clockRunning = false;
-  var timeRemain = 60;
+  var timeRemain = 15;
+
+	var numberCorrect = 0;
+	var numberIncorrect = 0;
+var numberUnanswered = 0;
 
   $("#startquiz").on("click", function() {
   	start();
@@ -29,59 +33,45 @@ $(document).ready(function() {
   function stop() {
     clearInterval(intervalId);
     clockRunning = false;
+    $('#listofQs').hide();
+    $('#results').show();
+    checkAnswers();
   };
 
 	function count () {
 		timeRemain--;
 		$('#timer').html(' Time Remaining: ' + timeRemain);
 		console.log(timeRemain)
+
+        if(timeRemain <= 0){
+        	stop();
+        }
 	};
 
-var numberCorrect = 0;
-var numberIncorrect = 0;
-var numberUnanswered = 0;
+	function checkAnswers(){
+		var answers = $('input:checked');
+		answers.each(function(index, ele){
+			console.log(ele)
+			if(ele.getAttribute('correct') == "true"){
+				console.log("Yes");
+			}else{
+				console.log("No");
+			}
+		});
+	}
 
 
-var questions = [{
-	question: "What team did Babe Ruth play for before joining the Boston Red Sox?",
-	answers: ["New York Yankees", "Cincinnati Reds", "Baltimore Orioles", "Boston Braves"],
-	correctAnswer: "Baltimore Orioles",
-}, {
-	question: "What team moved to D.C. to become the Washington Nationals in 2005?",
-	answers: ["Montreal Expos", "Brooklyn Dodgers", "Kansas City Monarchs", "Tokyo Gaints"],
-	correctAnswer: "Montreal Expos",
-}, {
-	question: "What player currently holds the record for most home runs in a season?",
-	answers: ["Roger Maris", "Mark McGwire", "Babe Ruth", "Barry Bonds"],
-	correctAnswer: "Barry Bonds",
-}, {
-	question: "What is the oldest active ball park in Major League Baseball?",
-	answers: ["Fenway Park", "Wrigley Field", "Busch Stadium", "Minute Maid Park"],
-	correctAnswer: "Fenway Park",
-}, {
-	question: "What team won the 2017 World Series?",
-	answers: ["Los Angeles Dodgers", "New York Yankess", "Houston Astros", "St Louis Cardinals"],
-	correctAnswer: "Houston Astros",
-}, {
-	question: "What basketball great made the decision to switch sports, signing a minor league baseball contract with the Chicago White Sox in 1994?",
-	answers: ["Bo Jackson", "Michael Jordan", "Allen Iverson", "Karl Malone"],
-	correctAnswer: "Michael Jordan",
-}, {
-	question: "What country has contributed the most players to Major League Baseball outside of the United States?",
-	answers: ["Dominican Republic", "Mexcio", "Cuba", "Japan"],
-	correctAnswer: "Dominican Republic",
-}, {
-	question: "Where is the National Baseball Hall of Fame and Museum located?",
-	answers: ["Omaha, NE", "Springfield, MA", "Canton, OH", "Cooperstown, NY"],
-	correctAnswer: "Cooperstown, NY",
-}, {
-	question: "Where was the first Major League Baseball All-Star Game played?",
-	answers: ["Los Angeles, CA", "New York, NY", "Dallas, TX", "Chicago, IL"],
-	correctAnswer: "Chicago, IL",
-}, {
-	question: "Who holds the highest career batting average of all time?",
-	answers: ["Ted Williams", "Ty Cobb", "Jose Altuve", "Willie Mays"],
-	correctAnswer: "Houston Astros",
-}];
+// var theAnswers = [
+// "Baltimore Orioles",
+// "Montreal Expos",
+// "Barry Bonds",
+// "Fenway Park",
+// "Houston Astros",
+// "Michael Jordan",
+// "Dominican Republic",
+// "Cooperstown, NY",
+// "Chicago, IL",
+// "Houston Astros",
+// ];
 
 });
